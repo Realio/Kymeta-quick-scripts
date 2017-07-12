517880d5dbe0b7bcdba12ec5b1f712a9ff7deef7
Jeff Wilcox <realio@gmail.com>
2017-07-12 10:59:02 -0700

    Directory opens in network drive/2nd prompt starts where first ends

--- bestJsonMover.py
+++ bestJsonMover.py
@@ -14,9 +14,9 @@ from tkFileDialog import askdirectory
 json_list = []
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-my_values = askopenfilename(title = "Please choose your text file that contains the BEST serial numbers")
-my_path = askdirectory(title = "Please choose the directory where the optimized Jsons reside")
-my_destination = askdirectory(title = "Where do you want the chosen BEST Jsons to go?")
+my_values = askopenfilename(initialdir = "Desktop", title = "Please choose your text file that contains the BEST serial numbers")
+my_path = askdirectory(initialdir =  "K:\Public\Engineering\LabData\Ku Radial\Test Data\Ku 70cm\mTennaU7 (5.1 TFT)", title = "Please choose the directory where the optimized Jsons reside")
+my_destination = askdirectory(initialdir =  my_path, title = "Where do you want the chosen BEST Jsons to go?")
 print "Here is where you are getting your best serial numbers from:" + (my_values)
 print "Here is which files you are choosing from:" + (my_path)
 print "Here is where you are writing the files to:" + (my_destination)
